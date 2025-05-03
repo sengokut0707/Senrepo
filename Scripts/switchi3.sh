55917d0bff40149e3530adecb4876114dca6af4a
hevark1974 <hevark1974@gmail.com>
2025-05-03 10:15:18 +0900

    delete

--- Scripts/switchi3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo sed -i 's/^autologin-session=.*$/autologin-session=i3/' /etc/lightdm/lightdm.conf
--- Scripts/switchsway.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo sed -i 's/^autologin-session=.*$/autologin-session=sway/' /etc/lightdm/lightdm.conf
