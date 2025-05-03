cb8807abe56c1c2a3ac9b13dd631086e4e1c6f05
sengokut0707 <sengokut0707@gmail.com>
2025-05-03 15:28:40 +0900

    add make

--- arch-install/pacstarp-install.sh
+++ arch-install/pacstarp-install.sh
@@ -4,6 +4,8 @@
 mkdir /mnt/root
 
 mount "/dev/disk/by-label/ROOT_DISK" /mnt/root
+mkdir /mnt/root/boot
+mkdir /mnt/root/home
 mount "/dev/disk/by-label/BOOT_DISK" /mnt/root/boot
 mount "/dev/disk/by-label/HOME_DISK" /mnt/root/home
 
