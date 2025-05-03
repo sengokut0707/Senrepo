bf9439632793e0177dd6eb62ada2b62b68347e5e
sengokut0707 <sengokut0707@gmail.com>
2025-05-03 14:52:53 +0900

    add

---
+++ arch-install/make_partition.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#ls /sys/firmware/efi/
+#lsblk
+
+wipefs -a /dev/sda
+sgdisk --zap-all /dev/sda
+
+sgdisk -n 1:0:+1G   -t 1:ef00 -c 1:"BOOT_DISK" /dev/sda
+sgdisk -n 2:0:+30G  -t 2:8300 -c 2:"ROOT_DISK" /dev/sda
+sgdisk -n 3:0:0     -t 3:8300 -c 3:"HOME_DISK" /dev/sda
+
+partprobe /dev/sda
+sleep 2
+
+# 各パーティションをフォーマット
+mkfs.fat -F32 -n BOOT_DISK "/dev/sda1"
+mkfs.ext4     -L ROOT_DISK "/dev/sda2"
+mkfs.ext4     -L HOME_DISK "/dev/sda3"
+
+lsblk -f /dev/sda
