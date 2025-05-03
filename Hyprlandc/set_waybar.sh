9e1ac5e793f1ee9965f7d52c9f40ef0d4ed8c1e9
sengokut0707@gmail.com <sengokut0707@gmail.com>
2025-05-03 14:11:57 +0000

    waybar bug fix

--- Hyprlandc/set_waybar.sh
+++ Hyprlandc/set_waybar.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
-rm -vr ~/.config/waybar
-mkdir -p ~/.config/waybar
-cp -vr $SCRIPT_DIR/* ~/.config/waybar
+rm -vr ~/.config/waybar/
+mkdir -p ~/.config/waybar/
+cp -vr $SCRIPT_DIR/waybar/* ~/.config/waybar
