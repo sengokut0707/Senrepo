a2b436d773eee0f4ba4916e3cdb7c57b36cfe989
sengokut0707 <sengokut0707@gmail.com>
2025-05-18 14:59:25 +0900

    fcitx5

--- arch-install/set_Japanized.sh
+++ arch-install/set_Japanized.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 sudo pacman -S noto-fonts-cjk
-sudo pacman -S fcitx-im fcitx-mozc
-sudo pacman -S fcitx-configtool
+sudo pacman -S fcitx5 fcitx5-im fcitx5-mozc fcitx5-configtool fcitx5-gtk
+#sudo pacman -S fcitx-im fcitx-mozc
 #cat $SCRIPT_DIR/xprofile > ~/.xprofile
 echo ""
 echo "please set xprofile and fcitx-configtool setting mozc"
